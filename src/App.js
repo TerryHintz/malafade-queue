dafbec374b6a6b1131ab530aa44a5de160685d0a
Leo <leo@eazytex.com>
2019-10-05 15:48:13 -0700

    restructure to react

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Swal from 'sweetalert2';
 
+let list = [];
+let id = localStorage.getItem('id') || 0;
+if (id){
+    id = parseInt(id, 10);
+}
+
 class App extends Component {
 	state = {
-		todos: []
+		temp: true,
+        list: JSON.parse(localStorage.getItem('queue')) || [],
 	};
 
 	componentDidMount() {
-    
-  }
 
-  removeCustomer = (event) => {
+    }
+
+  removeCustomer = (id) => {
 
     (async () => {
       const { value: password } = await Swal.fire({
@@ -24,11 +31,22 @@ class App extends Component {
         }
       })
       
-      if (password === "lakers23") {
-        let queue = document.getElementById('queue');
-        queue.removeChild(event.target.previousElementSibling);
-        queue.removeChild(event.target.previousElementSibling);
-        queue.removeChild(event.target);
+      if (password === "") {
+        // let queue = document.getElementById('queue');
+        // let temp = document.getElementById(id);
+
+        // queue.removeChild(temp.parentNode);
+
+        id = parseInt(id, 10);
+        list = this.state.list;
+        list = list.filter(function(obj){
+            return obj.id !== id;
+          });
+
+        this.setState({list: list});
+
+          localStorage.setItem('queue', JSON.stringify(list));
+
       } else {
         Swal.fire({
           title: "Incorrect Password"
@@ -37,7 +55,7 @@ class App extends Component {
     })();
   }
 
-	handleconfirm = () => {
+	handleConfirm = () => {
     let customer_name = document.getElementById('input_name').value;
     let barber_name = document.getElementById('input_barber').value;
 
@@ -52,33 +70,21 @@ class App extends Component {
     document.getElementById('input_barber').value = '';
     document.getElementById('input_name').value = '';
 
-    var customer = document.createElement('span');
-    var barber = document.createElement('span');
-    var x_button = document.createElement('span');
+    list = this.state.list;
 
-    if (barber_name === ''){
-        customer.classList.add('barber-wait');
-        barber.classList.add('barber-wait');
-        customer.classList.add('queue-element');
-        barber.classList.add('queue-element');
-    } else {
-        customer.classList.add('barber-none');
-        barber.classList.add('barber-none');
-        customer.classList.add('queue-element');
-        barber.classList.add('queue-element');
-    }
+    list.push({
+        id: id,
+        name: customer_name,
+        barber: barber_name,
+    });
 
-    customer.innerHTML += customer_name;
-    barber.innerHTML += barber_name;
-    x_button.innerHTML += 'X';
+    id++;
 
-    x_button.classList.add('x-button');
-    x_button.addEventListener('click', this.removeCustomer);
+    localStorage.setItem('id', id);
 
-    let queue = document.getElementById('queue');
-    queue.appendChild(customer);
-    queue.appendChild(barber);
-    queue.appendChild(x_button);
+    localStorage.setItem('queue', JSON.stringify(list));
+
+    this.setState({list: list});
 
     Swal.fire({
       title: "Thank you, you have been added to the waitlist, please wait for us to call your name.",
@@ -88,56 +94,63 @@ class App extends Component {
 }
 
 	render() {
-		return (
-      <div>
-			<div>
-    <link rel="stylesheet" type="text/css" href="css/main.css"></link>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"></link>
-</div>
+        return (
+            <div>
+			    <div>
+                    <link rel="stylesheet" type="text/css" href="css/main.css"></link>
+                    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"></link>
+                </div>
 
-<div className='body'>
-<img className='logo' src="http://www.malafadebarbershop.com/uploads/7/9/7/9/79799084/published/9475109_1.png?1542419052" alt="logo"></img>
-<div className='grid-2'>
-    <div id='queue' className ='grid-queue'>
-        <span className='sub-title'>Customer</span>
-        <span className='sub-title'>Barber</span>
-        <span></span>
-    </div>
-    <div className = 'customer-input'>
-        
-                <div className='request'>Please enter your fullname</div>
-                <div className='input-field'><input type="text" id="input_name"></input></div>
-        
-        <div>
-            <div className='request'>Waiting for a specific barber?</div>
-            <div className='request'>If yes, enter their name</div>
-            <div className='request'>If no, leave blank</div>
-            <div className='input-field'><input type="text" id="input_barber"></input></div>
-
-            <div className='button-confirm-div' onClick={() => this.handleconfirm()}>
-                <button id='btn_submit' className='button-confirm-a'>
-                    <span className='rat'>Confirm</span>
-                </button>
+                <div className='body'>
+                    <img className='logo' src="http://www.malafadebarbershop.com/uploads/7/9/7/9/79799084/published/9475109_1.png?1542419052" alt="logo"></img>
+                    <div className='grid-2'>
+                        <div id='queue'>
+                            <div className ='grid-queue'>
+                                <span className='sub-title'>Customer</span>
+                                <span className='sub-title'>Barber</span>
+                                <span></span>
+                            </div>
+                                {(this.state.list).map((arr) => {
+                                    return(
+                                        <div className ='grid-queue'>
+                                            {arr.barber ? <span className='queue-element barber-wait'>{arr.name}</span> : <span className='queue-element barber-none'>{arr.name}</span>}
+                                            {arr.barber ? <span className='queue-element barber-wait'>{arr.barber}</span> : <span className='queue-element barber-none'>{arr.barber}</span>}
+                                            <span id={arr.id} className={'x-button'} onClick={() => this.removeCustomer(arr.id)}>X</span>
+                                        </div>
+                                    );
+                                })}
+                        </div>
+                        <div className = 'customer-input'>
+                            <div className='request'>Please enter your fullname</div>
+                            <div className='input-field'><input type="text" id="input_name"></input></div>
+                            <div>
+                                <div className='request'>Waiting for a specific barber?</div>
+                                <div className='request'>If yes, enter their name</div>
+                                <div className='request'>If no, leave blank</div>
+                                <div className='input-field'><input type="text" id="input_barber"></input></div>
+                                <div className='button-confirm-div'>
+                                    <button id='btn_submit' className='button-confirm-a' onClick={() => this.handleConfirm()}>
+                                        <span className='rat'>Confirm</span>
+                                    </button>
+                                </div>
+                                <div className='sub-title'>Services</div>
+                                <div className='services'>Fades . 20</div>
+                                <div className='services'>Adult Haircut . 17</div>
+                                <div className='services'>Seniors . 14</div>
+                                <div className='services'>Children . 14</div>
+                                <div className='services'>Dry Shave . 12</div>
+                                <div className='services'>Hot Shave . 22</div>
+                                <div className='services'>Beard Trim . 6</div>
+                                <div className='services'>Beard Line-Up . 6</div>
+                                <div className='services'>Black Mask . 6</div>
+                                <div className='services'>Beard Colour . 12</div>
+                                <div className='services'>Hair Black Colour . 20</div>
+                                <div className='services'>Hair Design . 10+</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div className='sub-title'>Services</div>
-            <div className='services'>Fades . 20</div>
-            <div className='services'>Adult Haircut . 17</div>
-            <div className='services'>Seniors . 14</div>
-            <div className='services'>Children . 14</div>
-            <div className='services'>Dry Shave . 12</div>
-            <div className='services'>Hot Shave . 22</div>
-            <div className='services'>Beard Trim . 6</div>
-            <div className='services'>Beard Line-Up . 6</div>
-            <div className='services'>Black Mask . 6</div>
-            <div className='services'>Beard Colour . 12</div>
-            <div className='services'>Hair Black Colour . 20</div>
-            <div className='services'>hair Design . 10+</div>
-        </div>
-    </div>
-</div>
-</div>
-</div>
 		);
 	}
 }
